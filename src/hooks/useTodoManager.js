c1b384db05b38488ddb313d8f2f7b222eb7db25d
Mark RUbanenko <m.rubanenko14@gmail.com>
2025-12-16 19:54:56 +0100

    extract useTodoAction from useTodoManager

--- src/hooks/useTodoManager.js
+++ src/hooks/useTodoManager.js
@@ -7,6 +7,7 @@ import {
 } from "../helpers/todoHelpers";
 import { loadFromLocalStorage, savedToLocalStorage } from "../helpers/storage";
 import { createTodo, deleteTodo, fetchTodos, updateTodo } from "../api/todoApi";
+import { useTodoActions } from "./useTodoAction";
 
 export const useTodoManager = () => {
   const [todos, setTodos] = useState([]);
@@ -31,150 +32,18 @@ export const useTodoManager = () => {
     loadInitialData();
   }, []);
 
-  const onAdd = async (text, deadline) => {
-    const newTodo = createNewTodo(text, deadline, todos.length + 1);
-
-    const updatedTodos = [...todos, newTodo];
-    setTodos(updatedTodos);
-    savedToLocalStorage(updatedTodos);
-
-    try {
-      const createdTodo = await createTodo(newTodo);
-
-      const syncedTodos = updatedTodos.map((todo) =>
-        todo.id === newTodo.id ? createdTodo : todo
-      );
-
-      setTodos(syncedTodos);
-      savedToLocalStorage(syncedTodos);
-    } catch (error) {
-      console.error("Adding Error", error);
-      setTodos(todos);
-    }
-  };
-
-  const handleUpdate = async (id, text, deadline) => {
-    const todoToUpdate = todos.find((todo) => todo.id === id);
-
-    if (!todoToUpdate) return;
-
-    const updatedTodo = updateTodoData(todoToUpdate, text, deadline);
-    const updatedTodos = todos.map((todo) =>
-      todo.id === id ? updatedTodo : todo
-    );
-
-    setTodos(updatedTodos);
-
-    try {
-      await updateTodo(id, updatedTodo);
-      savedToLocalStorage(updatedTodos);
-    } catch (error) {
-      console.log("Update Error", error);
-      setTodos(todos);
-    }
-  };
-
-  const toggleComplete = async (id) => {
-    const todoToUpdate = todos.find((todo) => todo.id === id);
-
-    if (!todoToUpdate) return;
-
-    const updatedTodo = toggleTodoCompletion(todoToUpdate);
-
-    const updatedTodos = todos.map((todo) =>
-      todo.id === id ? updatedTodo : todo
-    );
-
-    setTodos(updatedTodos);
-
-    try {
-      await updateTodo(id, updatedTodo);
-
-      savedToLocalStorage(updatedTodos);
-    } catch (error) {
-      console.log("Update Error", error);
-      setTodos(todos);
-    }
-  };
-
-  const handleDelete = async (id) => {
-    const previousTodos = todos;
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
-
-    try {
-      await deleteTodo(id);
-      savedToLocalStorage(updatedTodos);
-    } catch (error) {
-      console.error("Delete Error", error);
-      setTodos(previousTodos);
-    }
-  };
-
-  const hasCompletedTodos = todos.some((todo) => todo.completed);
-
-  const handleDeleteCompleted = () => {
-    if (!hasCompletedTodos) return;
-    setIsDeletingCompleted(true);
-  };
-
-  const confirmDeleteCompleted = async () => {
-    const originalTodos = [...todos];
-
-    const completedIds = originalTodos
-      .filter((t) => t.completed)
-      .map((t) => t.id);
-
-    setTodos(originalTodos.filter((todo) => !todo.completed));
-
-    const failedIds = [];
-
-    for (const id of completedIds) {
-      try {
-        await deleteTodo(id);
-      } catch (error) {
-        console.error(`Delete Task Error ${id}:`, error);
-        failedIds.push(id);
-      }
-    }
-
-    if (failedIds.length > 0) {
-      setTodos(
-        originalTodos.filter(
-          (todo) => !todo.completed || failedIds.includes(todo.id)
-        )
-      );
-    }
-
-    savedToLocalStorage(todos);
-    setIsDeletingCompleted(false);
-  };
-  const onOrder = (activeId, overId) => {
-    if (!overId) return;
-
-    const activeIndex = todos.findIndex((todo) => todo.id === activeId);
-    const overIndex = todos.findIndex((todo) => todo.id === overId);
-    if (activeIndex === -1 || overIndex === -1 || activeIndex === overIndex)
-      return;
-
-    const newTodos = [...todos];
-    const [movedTodo] = newTodos.splice(activeIndex, 1);
-    newTodos.splice(overIndex, 0, movedTodo);
-
-    const updatedTodos = newTodos.map((todo, index) => ({
-      ...todo,
-      order: index + 1,
-    }));
-
-    setTodos(updatedTodos);
-    savedToLocalStorage(updatedTodos);
-
-    updatedTodos.forEach((todo) => {
-      updateTodo(todo.id, { order: todo.order }).catch((err) =>
-        console.log("Update order failed", err)
-      );
-    });
-  };
+  const actions = useTodoActions({
+    todos,
+    setTodos,
+    createNewTodo,
+    createTodo,
+    savedToLocalStorage,
+    updateTodoData,
+    updateTodo,
+    toggleTodoCompletion,
+    deleteTodo,
+    setIsDeletingCompleted,
+  });
 
   return {
     todos,
@@ -182,13 +51,6 @@ export const useTodoManager = () => {
     setDeletingId,
     setIsDeletingCompleted,
     isDeletingCompleted,
-    onAdd,
-    handleUpdate,
-    toggleComplete,
-    handleDelete,
-    handleDeleteCompleted,
-    hasCompletedTodos,
-    confirmDeleteCompleted,
-    onOrder,
+    ...actions,
   };
 };
